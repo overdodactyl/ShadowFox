746e2caf5c327a3b75451c7a8ac6755c9c9dee31
Shourai <10200748+Shourai@users.noreply.github.com>
2018-03-11 15:53:56 +0100

    More thorough regex for internal UUID finder

--- internal_UUID_finder.sh
+++ internal_UUID_finder.sh
@@ -31,7 +31,7 @@ IFS=',' read -ra EXTS <<< "$line"
 for i in "${EXTS[@]}"; do
     id=$(echo $i | sed -n 's/.*"\(.*\)\\":.*/\1/p')
     uuid=$(echo $i | sed -n 's/.*"\(.*\)\\".*/\1/p')
-    if test "${styled[$id]+isset}"
+    if [[ -n "${styled[$id]}" ]]
     then
         echo "${styled[$id]}_UUID=$uuid" >> 'ShadowFox_customization/internal_UUIDs.txt'
     fi;
