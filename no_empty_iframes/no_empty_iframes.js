4c25c4f1a14f0515e7950c25046629db689e2347
overdodactyl <patjohns1216@gmail.com>
2018-11-24 15:31:42 -0700

    no_empty_iframes addon #222

---
+++ no_empty_iframes/no_empty_iframes.js
@@ -0,0 +1,12 @@
+/* Once page finishes loading, find all empty iframes
+ * and add empty div inside */
+window.onload = function() { 
+  var frames = document.getElementsByTagName("iframe");
+  for (var i = 0; i < frames.length; i++) {
+    var doc = frames[i].contentWindow.document;
+    if (doc.body.innerHTML === "") {
+    	var elemDiv = document.createElement('div');
+    	doc.body.appendChild(elemDiv);
+    }
+  }
+}
